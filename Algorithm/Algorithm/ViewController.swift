5bb241117ec36e16e7e55d4c4b6a74f861bd47db
Saurav Satpathy <ssatpathy@wbhq.com>
2018-07-13 21:17:26 +0530

    Created new proj

---
+++ Algorithm/Algorithm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorithm
+//
+//  Created by 703177729 on 13/07/18.
+//  Copyright © 2018 com.fsfes.frewfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
