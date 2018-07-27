e6a3d7419ccf97f8d626340c53ef87409da71bd2
Saurav Satpathy <ssatpathy@wbhq.com>
2018-07-27 19:16:22 +0530

    Added linked list insert

--- Algorithm/Algorithm/DataStructure/LinkedList.swift
+++ Algorithm/Algorithm/DataStructure/LinkedList.swift
@@ -9,5 +9,44 @@
 import UIKit
 
 class LinkedList: NSObject {
-
+    
+    private (set) var head: Node?
+    private (set) var size: uint = 0
+    
+    class Node {
+        var data: Int?
+        var prev: Node?
+        var next: Node?
+        
+        class func create(data: Int) -> Node {
+            let node = Node.init()
+            node.data = data
+            return node
+        }
+    }
+    
+    func isEmpty() -> Bool {
+        return size > 0
+    }
+    
+    func insert(data: Int) {
+        let node = Node.create(data: data)
+        if head == nil{
+            head = node
+            head?.prev = node
+            head?.next = node
+        }else{
+            head = node
+            head?.prev = node
+            head?.next = node
+        }
+    }
+    
+    func delete(data: Int) {
+        
+    }
+    
+    
+    
+    
 }
