b2c6b7e721713904d3773cfb00ea4f980893643b
Saurav Satpathy <ssatpathy@wbhq.com>
2019-05-06 20:27:29 +0530

    added comments

--- Algorithm/Algorithm/Sorting/MergeSort.swift
+++ Algorithm/Algorithm/Sorting/MergeSort.swift
@@ -5,6 +5,18 @@
 //  Created by Saurav Satpathy on 17/07/18.
 //  Copyright © 2018 com.bitMountn.Algorithm. All rights reserved.
 //
+/*
+ Idea:
+ Divide the unsorted list into sublists, each containing single element. By rule single element array is sorted.
+ Take adjacent single element lists comapre and merge them. Repeat the process till a single sorted list of obtained.
+ 
+ 
+ Description:
+ Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves,
+ sorts the two halves and merges the sorted halves.
+ 
+ 
+ */
 
 import UIKit
 
@@ -26,12 +38,13 @@ class MergeSort: NSObject {
         }
     }
     
+    // Q: Why name Merge() when it comapres also?
+    // Ans: Merge is actually an algorithm. It takes sorted lists as input and produce a single list as output in sorted order.
     func merge(arr: inout [Int], low: Int, mid: Int, high: Int) {
         var tempArr: [Int] = arr
         var i = low
-        var j = mid + 1
         var k = low
-        
+        var j = mid + 1
         
         while i <= mid && j <= high {
             if tempArr[i] <= tempArr[j]{
